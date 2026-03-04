f438700c5a5582a206f828678e3b22f138ef2305
Erick Silva <erick_novo@hotmail.com>
2026-03-04 19:03:57 -0300

    fix backward propagation - dW was using dQ instead of Q

--- engine.py
+++ engine.py
@@ -2,17 +2,18 @@ import numpy as np
 
 
 class NNEngine:
-    def __init__(self, input_data, n_layers, hidden_layers_dim, activations, classification=True):
+    def __init__(self, x, y, n_layers, hidden_layers_dim, activations, classification=True):
         self.classification = classification
         self.n_layers = n_layers
         self.hidden_layers_dim = hidden_layers_dim
 
         assert len(self.hidden_layers_dim) == self.n_layers - 1, "The length of the parameter hidden_layers_dim must match the number of hidden layers"
 
-        self.activations = activations
+        self.activations_names = activations
+        self.activations_values = None
 
-        self.x = input_data[:, :-1]  # dimensions = (a,b), where a=number of examples and b=number of features
-        self.y = input_data[:, -1]  # dimensions = (a,) - output vector
+        self.x = x  # dimensions = (a,b), where a=number of examples and b=number of features
+        self.y = y  # dimensions = (a,) - output vector
 
         self.weights = None  # dimensions = (a,b,c), where c=number of layers
         self.bias = None  # dimensions = (a,c)
@@ -24,27 +25,28 @@ class NNEngine:
 
     def init_params(self, classes=None):
         shape = (self.hidden_layers_dim[0], self.x.shape[1])
-        self.weights = [np.random.normal(0, self._weight_init(self.activations[0], shape), size=shape)]
+        self.weights = [np.random.normal(0, self._weight_init(self.activations_names[0], shape), size=shape)]
         self.bias = [np.zeros((self.hidden_layers_dim[0], 1))]
 
         for layer in range(1, self.n_layers - 1):
             shape = (self.hidden_layers_dim[layer], self.hidden_layers_dim[layer - 1])
-            self.weights.append(np.random.normal(0, self._weight_init(self.activations[0], shape), size=shape))
+            self.weights.append(np.random.normal(0, self._weight_init(self.activations_names[0], shape), size=shape))
             self.bias.append(np.zeros((self.hidden_layers_dim[layer], 1)))
 
         # last layer
         shape = (1 if classes is None else classes, self.hidden_layers_dim[self.n_layers - 2])
-        self.weights.append(np.random.normal(0, self._weight_init(self.activations[0], shape), size=shape))
+        self.weights.append(np.random.normal(0, self._weight_init(self.activations_names[1], shape), size=shape))
         self.bias.append(np.zeros((1 if classes is None else classes, 1)))
 
-        self.zero_grad()
+        self.dW = [np.zeros_like(array) for array in self.weights]
+        self.db = [np.zeros_like(array) for array in self.bias]
 
         self.Z = [np.zeros_like(array) for array in self.bias]
 
     @staticmethod
     def _weight_init(activation, shape):
         if activation == 'relu':
-            return np.sqrt(2 / shape[1])  # uses He initialization
+            return np.sqrt(2 / shape[1])  # uses He initialization (specific for ReLU activation)
         return np.sqrt(2 / (shape[0] + shape[1]))  # uses Xavier initialization
 
     def forward_propagation(self, batch_size=None):
@@ -55,13 +57,15 @@ class NNEngine:
             xb, yb = self.x[ri], self.y[ri]
 
         q = xb
+        self.activations_values = [q]
         for n in range(0, self.n_layers):
-            self.Z[n] = q.dot(self.weights[n].T) + self.bias[n].T  # TODO: transpose weights and bias before this operation
+            self.Z[n] = q.dot(self.weights[n].T) + self.bias[n].T
             if n != self.n_layers - 1:
-                q = self.activation_function(self.Z[n], self.activations[0])
+                q = self.activation_function(self.Z[n], self.activations_names[0])
+                self.activations_values.append(q)  # storing activations to use on backward propagation
 
         # final layer transformation
-        y_pred = self.activation_function(self.Z[self.n_layers - 1], self.activations[1 if len(self.activations) > 1 else 0])
+        y_pred = self.activation_function(self.Z[self.n_layers - 1], self.activations_names[1 if len(self.activations_names) > 1 else 0])
 
         if self.classification:
             perf = self.accuracy(yb, y_pred)
@@ -73,22 +77,23 @@ class NNEngine:
         return cost, perf, yb, y_pred
 
     def backward_propagation(self, y, y_pred):
-        n = self.y.size  # mini-batch size
+        n = y.size  # mini-batch size
 
         if self.classification:
             dZ = y_pred - self._y_transform(y)  # derivative of the cost function w.r.t. z
         else:
             dZ = 2 * (y_pred - y.reshape(-1, 1))
 
-        dq = 1
         for l in range(self.n_layers - 1, -1, -1):
             # chain rule makes it easier to compute the derivatives
-            if l != self.n_layers - 1:
-                dZ = dq * self.derivActivation(self.Z[l], name=self.activations[0])
-            dq = dZ.dot(self.weights[l])
+            a_prev = self.activations_values[l]
 
-            self.dW[l] = (1 / n) * dZ.T.dot(dq)
-            self.db[l] = (1 / n) * dZ.sum(axis=0).T.reshape(-1, 1)
+            # calculate gradients for weights and bias
+            self.dW[l] = (1 / n) * (dZ.T @ a_prev)
+            self.db[l] = (1 / n) * np.sum(dZ, axis=0, keepdims=True).T
+
+            if l > 0:
+                dZ = (dZ @ self.weights[l]) * self.derivActivation(self.Z[l - 1], name=self.activations_names[0])
 
     # for classification: transform the y output into a matrix of entries (a, M) where M is the number os classes
     @staticmethod
@@ -103,10 +108,6 @@ class NNEngine:
             self.weights[layer] -= learning_rate * self.dW[layer]
             self.bias[layer] -= learning_rate * self.db[layer]
 
-    def zero_grad(self):
-        self.dW = [np.zeros_like(array) for array in self.weights]
-        self.db = [np.zeros_like(array) for array in self.bias]
-
 
     """ activation functions below """
     def activation_function(self, v, name=None):
@@ -114,6 +115,8 @@ class NNEngine:
             return self._relu(v)
         if name == 'softmax':
             return self._softmax(v)
+        if name == 'tanh':
+            return self._tanh(v)
         return v
 
     @staticmethod
@@ -122,7 +125,9 @@ class NNEngine:
 
     @staticmethod
     def _softmax(v):
-        return np.exp(v) / sum(np.exp(v))
+        #return np.exp(v) / sum(np.exp(v))
+        exps = np.exp(v - np.max(v, axis=1, keepdims=True))
+        return exps / np.sum(exps, axis=1, keepdims=True)
 
     @staticmethod
     def _tanh(v):
@@ -143,13 +148,12 @@ class NNEngine:
         return sum((y - y_p.ravel()) ** 2) * (1 / n) if n > 0 else None
 
     # categorical cross-entropy
-    @staticmethod
-    def cce_cost(y, y_p):
-        return -sum(sum(y * np.log(y_p)))
+    def cce_cost(self, y, y_p):
+        return -sum(sum(self._y_transform(y) * np.log(np.clip(y_p, 1e-15, 1.0))))
 
 
     """ performance evaluation functions """
     @staticmethod
     def accuracy(y, y_p):  # for classification problems
-        y_p = np.argmax(y_p, 0)
+        y_p = np.argmax(y_p, 1)
         return np.sum(y_p == y) / y.size
--- training.py
+++ training.py
@@ -1,37 +1,46 @@
 import numpy as np
 from engine import NNEngine
+from sklearn.datasets import fetch_openml
 
+# mnist = fetch_openml('mnist_784', version=1, as_frame=False)
+#
+# x, y = mnist["data"], mnist["target"]
+#
+# x = x.astype('float32') / 255.0
+# y = y.astype('int32')
 
 x = np.array([[2.0, 3.0, -1.0], [3.0, -1.0, 0.5], [0.5, 1.0, 1.0], [1.0, 1.0, -1.0]])
-x_normalized = (x - np.mean(x)) / np.std(x)
 y = np.array([1.0, -1.0, -1.0, 1.0])
-#y = np.array([10, 20, 30, 40])
-input_data = np.hstack((x_normalized, y.reshape(-1, 1)))
+
 
 n_layers = 2
 hidden_layers_dim = [10]  # number of neurons in each hidden layer. len = n_layers - 1
+#activation_funcs = ['relu', 'softmax']
 activation_funcs = ['tanh', None]
 
 np.random.seed(100)
 
 
-engine = NNEngine(input_data, n_layers, hidden_layers_dim, activation_funcs, classification=False)
+# engine = NNEngine(x, y, n_layers, hidden_layers_dim, activation_funcs, classification=True)
+# engine.init_params(classes=10)
+engine = NNEngine(x, y, n_layers, hidden_layers_dim, activation_funcs, classification=False)
 engine.init_params()
 
 
-for k in range(10000):
-
+for k in range(100):
     # forward
+    #cost, perf, y, y_pred = engine.forward_propagation(batch_size=64)
     cost, perf, y, y_pred = engine.forward_propagation()
 
     # backward
-    engine.zero_grad()
     engine.backward_propagation(y, y_pred)
 
     # update weights
-    # learning_rate = 1.0 - 0.9 * (k + 1) / 100
-    learning_rate = 0.0001
+    #learning_rate = 1.0 - 0.9 * k / 100
+    learning_rate = 0.01
     engine.gradient_descent(learning_rate)
 
-    # print(f"step {k}. loss={cost}; perf={cost}; y_pred={y_pred.ravel()}")
-    print(f"step {k}. loss={cost}; y_pred={y_pred.ravel()}")
+    #print(f"step {k}. cost={cost}; acc={perf}")
+    print(f"step {k}. cost={cost}; y_pred={y_pred.ravel()}")
+
+    engine.activations_values = None
